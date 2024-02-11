e802a4a0fe9848967b3721e1b0d7b3878a8e2724
Diana Lyvytska <dianalyvytska@gmail.com>
2024-02-11 14:29:15 +0200

    improve canvas moving

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -16,9 +16,9 @@ interface CanvasProps {
   images: ImageData[];
 }
 
-async function updatePhotoPosition(id: string, x:number, y:number) {
-  const docRef = doc(db, 'photos', id)
-  await updateDoc(docRef, {x, y, order: Date.now()})
+async function updatePhotoPosition(id: string, x: number, y: number) {
+  const docRef = doc(db, 'photos', id);
+  await updateDoc(docRef, { x, y, order: Date.now() });
 }
 
 type PreloadedImages = { [key: string]: HTMLImageElement };
@@ -30,7 +30,8 @@ const Canvas: React.FC<CanvasProps> = ({ images }) => {
   const [dragStartCoords, setDragStartCoords] = useState<{ x: number; y: number } | null>(null);
   const [imagesLoaded, setImagesLoaded] = useState<boolean>(false);
   const [preloadedImages, setPreloadedImages] = useState<PreloadedImages>({});
-
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+  const [canvasOffset, setCanvasOffset] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -73,14 +74,22 @@ const Canvas: React.FC<CanvasProps> = ({ images }) => {
       context.clearRect(0, 0, canvas.width, canvas.height);
       imagePositions.forEach((imageData) => {
         const image = preloadedImages[imageData.id];
-        context.drawImage(image, imageData.x, imageData.y, imageData.width, imageData.height);
+        context.drawImage(
+          image,
+          imageData.x + canvasOffset.x,
+          imageData.y + canvasOffset.y,
+          imageData.width,
+          imageData.height
+        );
       });
     };
 
     const handleMouseDown = (e: MouseEvent<HTMLCanvasElement>) => {
-      const mouseX = e.clientX - canvas.getBoundingClientRect().left;
-      const mouseY = e.clientY - canvas.getBoundingClientRect().top;
-      console.log('imagPo', imagePositions);
+      const mouseX = e.clientX - canvas.getBoundingClientRect().left - canvasOffset.x;
+      const mouseY = e.clientY - canvas.getBoundingClientRect().top - canvasOffset.y;
+
+      // Check if any image is clicked
+      let imageClicked = false;
       for (let i = imagePositions.length - 1; i >= 0; i--) {
         const imageData = imagePositions[i];
 
@@ -90,24 +99,32 @@ const Canvas: React.FC<CanvasProps> = ({ images }) => {
           mouseY >= imageData.y &&
           mouseY <= imageData.y + imageData.height
         ) {
-          setSelectedImageIndex(imagePositions.length-1);
-
           // Reorder the images so that the selected image is moved to the end of the array
           const updatedImagePositions = [...imagePositions];
           updatedImagePositions.push(updatedImagePositions.splice(i, 1)[0]);
           setImagePositions(updatedImagePositions);
+          setSelectedImageIndex(imagePositions.length-1);
 
           setDragStartCoords({ x: mouseX - imageData.x, y: mouseY - imageData.y });
+          imageClicked = true;
           break;
         }
+      }
+
+      // If no image is clicked, initiate hand-dragging
+      if (!imageClicked) {
+        setIsDragging(true);
+        setDragStartCoords({ x: mouseX, y: mouseY });
       }
     };
 
     const handleMouseMove = (e: MouseEvent<HTMLCanvasElement>) => {
       if (selectedImageIndex !== null && dragStartCoords) {
-        const rect = canvas.getBoundingClientRect();
-        const mouseX = e.clientX - rect.left;
-        const mouseY = e.clientY - rect.top;
+        // Move the selected image
+        const mouseX = e.clientX - canvas.getBoundingClientRect().left - canvasOffset.x;
+        const mouseY = e.clientY - canvas.getBoundingClientRect().top - canvasOffset.y;
+
+        console.log(mouseX, canvasOffset);
 
         const newX = mouseX - dragStartCoords.x;
         const newY = mouseY - dragStartCoords.y;
@@ -115,16 +132,32 @@ const Canvas: React.FC<CanvasProps> = ({ images }) => {
         const newImagePositions = [...imagePositions];
         newImagePositions[selectedImageIndex] = { ...newImagePositions[selectedImageIndex], x: newX, y: newY };
         setImagePositions(newImagePositions);
+      } else if (isDragging && dragStartCoords) {
+        // Hand-drag the canvas
+        const mouseX = e.clientX - canvas.getBoundingClientRect().left - canvasOffset.x;
+        const mouseY = e.clientY - canvas.getBoundingClientRect().top - canvasOffset.y;
+
+
+        const offsetX = mouseX - dragStartCoords.x;
+        const offsetY = mouseY - dragStartCoords.y;
+
+        setCanvasOffset((prevOffset) => ({
+          x: prevOffset.x + offsetX,
+          y: prevOffset.y + offsetY,
+        }));
       }
     };
 
     const handleMouseUp = () => {
-      const {id, x, y} = imagePositions[selectedImageIndex];
-      updatePhotoPosition(id, x, y);
+      if(selectedImageIndex) {
+        const { id, x, y } = imagePositions[selectedImageIndex];
+        updatePhotoPosition(id, x, y);
+      }
       setSelectedImageIndex(null);
       setDragStartCoords(null);
-
+      setIsDragging(false);
     };
+
 
     const animate = () => {
       requestAnimationFrame(animate);
@@ -142,9 +175,9 @@ const Canvas: React.FC<CanvasProps> = ({ images }) => {
       canvas.removeEventListener('mousemove', handleMouseMove);
       canvas.removeEventListener('mouseup', handleMouseUp);
     };
-  }, [imagePositions, selectedImageIndex, dragStartCoords, imagesLoaded]);
+  }, [imagePositions, imagesLoaded, canvasOffset, isDragging, dragStartCoords]);
 
-  return <canvas ref={canvasRef} width={1000} height={1000} />;
+  return <canvas ref={canvasRef} width={800} height={800} style={{ cursor: isDragging ? 'grabbing' : 'grab' }} />;
 };
 
 export default Canvas;
