6cb91b1de63f50fe8fb8ac38f5387c6a181e6fad
Diana Lyvytska <dianalyvytska@gmail.com>
2024-04-04 23:58:10 +0300

    side panel

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import ImageIcon from './ImageIcon.tsx'
+import PhotosIcon from './PhotosIcon.tsx'
+import SettingsIcon from './SettingsIcon.tsx'
+import SlidersIcon from './SlidersIcon.tsx'
+import StickerIcon from './StickerIcon.tsx'
+
+export { ImageIcon, PhotosIcon, SettingsIcon, SlidersIcon, StickerIcon }
--- src/components/SidePanel/SidePanel.tsx
+++ src/components/SidePanel/SidePanel.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-import imageIcon from 'assets/image-icon.svg'
+import { ImageIcon, SettingsIcon, SlidersIcon, StickerIcon, PhotosIcon } from 'assets'
 
 import { Uploaded } from './sections'
 
@@ -8,7 +8,37 @@ enum Sections {
   uploaded = 'photos',
   stickers = 'stickers',
   backgrounds = 'backgrounds',
+  general = 'general',
+  editing = 'editing',
 }
+
+const sections = [
+  {
+    id: Sections.general,
+    icon: SettingsIcon,
+    title: 'General',
+  },
+  {
+    id: Sections.uploaded,
+    icon: PhotosIcon,
+    title: 'Photos',
+  },
+  {
+    id: Sections.stickers,
+    icon: StickerIcon,
+    title: 'Stickers',
+  },
+  {
+    id: Sections.backgrounds,
+    icon: ImageIcon,
+    title: 'Backgrounds',
+  },
+  {
+    id: Sections.editing,
+    icon: SlidersIcon,
+    title: 'Editing',
+  },
+] as const
 
 function SidePanel() {
   const [isExpanded, setIsExpanded] = useState(true)
@@ -19,36 +49,38 @@ function SidePanel() {
       case Sections.uploaded:
         return <Uploaded />
       default:
-        return <div></div>
+        return <></>
     }
   }
 
   return (
-    <div className='bg-neutral-800 w-[430px] h-screen fixed left-0 top-0 z-10 flex flex-row'>
-      <div className='bg-neutral-900 w-20 h-screen'>
-        <div
-          className={`w-20 h-20 text-xs flex flex-col items-center justify-center ${selectedSection === Sections.uploaded && 'bg-neutral-800'}`}
-          onClick={() => setSelectedSection(Sections.uploaded)}
-        >
-          <img src={imageIcon} className='w-[40px] h-[40px]' />
-          Photos
-        </div>
-        <div
-          className={`w-20 h-20 text-xs flex flex-col items-center justify-center ${selectedSection === Sections.stickers && 'bg-neutral-800'}`}
-          onClick={() => setSelectedSection(Sections.stickers)}
-        >
-          <img src={imageIcon} className='w-[40px] h-[40px]' />
-          Stickers
-        </div>
-        <div
-          className={`w-20 h-20 text-xs flex flex-col items-center justify-center ${selectedSection === Sections.backgrounds && 'bg-neutral-800'}`}
-          onClick={() => setSelectedSection(Sections.backgrounds)}
-        >
-          <img src={imageIcon} className='w-[40px] h-[40px]' />
-          Backgrounds
-        </div>
+    <div
+      className={`bg-neutral-800 h-screen fixed left-0 top-0 z-10 flex flex-row ${isExpanded ? 'w-[430px]' : 'w-20'}`}
+    >
+      <div
+        className='bg-neutral-800 absolute right-0 translate-x-1/2 h-14 w-6 top-1/2 -translate-y-1/2 rounded-xl flex items-center p-1 justify-end select-none -z-10'
+        onClick={() => setIsExpanded((isExpanded) => !isExpanded)}
+      >
+        <p>{isExpanded ? '<' : '>'}</p>
       </div>
-      <div className='grow p-4'>{renderSection()}</div>
+      <div className='bg-neutral-900 w-20 h-screen'>
+        {sections.map(({ id, title, icon: Icon }) => (
+          <div
+            key={id}
+            className={`w-20 h-20 text-xs flex flex-col items-center justify-center ${selectedSection === id && isExpanded && 'bg-neutral-800'}`}
+            onClick={() => {
+              setSelectedSection(id)
+              setIsExpanded(true)
+            }}
+          >
+            <div className='w-[40px] h-[40px]'>
+              <Icon />
+            </div>
+            {title}
+          </div>
+        ))}
+      </div>
+      {isExpanded && <div className='grow p-4'>{renderSection()}</div>}
     </div>
   )
 }
