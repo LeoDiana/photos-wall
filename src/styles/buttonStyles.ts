f8bbfd4b13b78cb62c66a8cdf05cae92edb402b0
Diana Lyvytska <dianalyvytska@gmail.com>
2024-04-30 23:38:22 +0300

    change button color

--- src/styles/buttonStyles.ts
+++ src/styles/buttonStyles.ts
@@ -1,3 +1,3 @@
 import tw from 'twin.macro'
 
-export const Button = tw.div`rounded-lg px-4 py-2 border-2 border-indigo-800 font-medium w-full bg-indigo-500 flex justify-center`
+export const Button = tw.div`rounded-lg px-4 py-2 border-2 border-orange-500 w-full bg-amber-500 flex justify-center text-neutral-800 font-semibold`
